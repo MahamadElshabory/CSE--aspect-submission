14ab8e7130cfe60cab56538610ec726e6a48c1e7
MahamadElshabory <mahamadd.shabory@gmail.com>
2025-03-05 00:39:57 +0200

    lab1

---
+++ lab1/src/main/java/com/example/demo/controller.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RestController
+public class controller {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from GET!";
+    }
+
+    @PostMapping("/hello")
+    public String sayPost() {
+        return "Hello from POST!";
+    }
+
+    @PutMapping("/hello")
+    public String sayPut() {
+        return "Hello from PUT!";
+    }
+
+    @DeleteMapping("/hello")
+    public String sayDelete() {
+        return "Hello from DELETE!";
+    }
+}
